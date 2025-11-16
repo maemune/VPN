f519e65c86be53f761169bb61ba70cd19c5802ad
maemune <maemune0515@gmail.com>
2025-11-16 12:20:56 +0900

    可読性の向上

--- VPN_Create.bat
+++ VPN_Create.bat
@@ -1,14 +1,26 @@
 @echo off
 
-REM "管理者として起動しているか確認"
+REM Check for Admin privileges
 whoami /priv | find "SeDebugPrivilege" > nul
 if %errorlevel% neq 0 (
 　@powershell start-process ”%~0" -verb runas
 　exit
 )
 
-REM "PowerShellコマンドでVPNの接続情報を設定し登録"
-REM "$vpnNameにVPN名を入力"
-REM "$serverAddressに接続先IPアドレスを入力"
-REM "-L2tpPskに事前共有鍵を入力"
-powershell -command "$vpnName = '######'; $serverAddress = '###.###.###.###'; Add-VpnConnection -Name $vpnName -ServerAddress $serverAddress -TunnelType L2tp -EncryptionLevel Maximum -AuthenticationMethod MSChapv2 -RememberCredential -SplitTunneling -L2tpPsk '#########' -Force; Get-VpnConnection -Name $vpnName"
+REM Configure and register VPN connection via PowerShell
+REM Set VPN Name
+REM Set Server IP Address
+REM Set Pre-Shared Key (PSK)
+
+powershell -command "$vpnName = '######'; ^
+$serverAddress = '###.###.###.###'; ^
+Add-VpnConnection -Name $vpnName ^
+-ServerAddress $serverAddress ^
+-TunnelType L2tp ^
+-EncryptionLevel Maximum ^
+-AuthenticationMethod MSChapv2 ^
+-RememberCredential ^
+-SplitTunneling ^
+-L2tpPsk '#########' ^
+-Force; ^
+Get-VpnConnection -Name $vpnName"
