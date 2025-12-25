6e5293c3b3123d952f871f05fb91af611ad70831
maemune <115972024+maemune@users.noreply.github.com>
2025-12-25 17:55:09 +0900

    Update VPN_Create.bat

--- VPN_Create.bat
+++ VPN_Create.bat
@@ -1,26 +1,6 @@
 @echo off
 
-REM Check for Admin privileges
-whoami /priv | find "SeDebugPrivilege" > nul
-if %errorlevel% neq 0 (
-　@powershell start-process ”%~0" -verb runas
-　exit
-)
+set VPN_NAME=############
+set SERVER_ADDR=###.###.###.###
 
-REM Configure and register VPN connection via PowerShell
-REM Set VPN Name
-REM Set Server IP Address
-REM Set Pre-Shared Key (PSK)
-
-powershell -command "$vpnName = '######'; ^
-$serverAddress = '###.###.###.###'; ^
-Add-VpnConnection -Name $vpnName ^
--ServerAddress $serverAddress ^
--TunnelType L2tp ^
--EncryptionLevel Maximum ^
--AuthenticationMethod MSChapv2 ^
--RememberCredential ^
--SplitTunneling ^
--L2tpPsk '#########' ^
--Force; ^
-Get-VpnConnection -Name $vpnName"
+powershell -command "$n='%VPN_NAME%'; $s='%SERVER_ADDR%'; $p='%PSK_KEY%'; Add-VpnConnection -Name $n -ServerAddress $s -SplitTunneling -Force; Get-VpnConnection -Name $n"
